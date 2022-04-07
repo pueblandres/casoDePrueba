358d7635ca0acd49e3d6257330b003648cdf979b
pueblandres <pueblandres@hotmail.com>
2022-04-07 18:12:41 -0300

    segundo commit

--- src/main/java/com/articulos/demo/DemoApplication.java
+++ src/main/java/com/articulos/demo/DemoApplication.java
@@ -4,8 +4,10 @@ package com.articulos.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class DemoApplication{
+public class DemoApplication   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
